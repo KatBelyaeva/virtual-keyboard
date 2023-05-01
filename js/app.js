c625aa7f66fa1738ca0582b85d60227f7ada7af9
KatBelyaeva <kat.belyaeva.it@gmail.com>
2023-05-01 23:19:38 +0300

    fix: fix bugs in capslock for mouse

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+import { keysEng, keyCode, keysRus } from './keys.js';
+
 const text = document.createElement('textarea');
 text.classList.add('keyboard-text');
 document.body.appendChild(text);
@@ -6,61 +8,168 @@ const keyboard = document.createElement('div');
 keyboard.classList.add('keyboard');
 document.body.appendChild(keyboard);
 
-const keys = [
-  '`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace',
-  'Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\', 'Del',
-  'CapsLock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', "'", 'Enter',
-  'Shift', '\\', 'z', 'x', 'c', 'v', 'b', 'n', 'm', '.', ',', '/', '↑', 'Shift',
-  'Ctrl', 'Win', 'Alt', 'Space', 'Alt', 'Ctrl', '←', '↓', '→'];
-
-const keyCode = [
-  'Backquote', 'Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5', 'Digit6', 'Digit7', 'Digit8', 'Digit9', 'Digit0', 'Minus', 'Equal', 'Backspace',
-  'Tab', 'KeyQ', 'KeyW', 'KeyE', 'KeyR', 'KeyT', 'KeyY', 'KeyU', 'KeyI', 'KeyO', 'KeyP', 'BracketLeft', 'BracketRight', 'Backslash', 'Delete',
-  'CapsLock', 'KeyA', 'KeyS', 'KeyD', 'KeyF', 'KeyG', 'KeyH', 'KeyJ', 'KeyK', 'KeyL', 'Semicolon', 'Quote', 'Enter',
-  'ShiftLeft', 'Backslash', 'KeyZ', 'KeyX', 'KeyC', 'KeyV', 'KeyB', 'KeyN', 'KeyM', 'Comma', 'Period', 'Slash', 'ArrowUp', 'ShiftRight',
-  'ControlLeft', 'MetaLeft', 'AltLeft', 'Space', 'AltRight', 'ControlRight', 'ArrowLeft', 'ArrowDown', 'ArrowRight'];
-
-function init() {
-  let out = '';
-  // eslint-disable-next-line no-plusplus
-  for (let i = 0; i < keys.length; i++) {
-    out += `<div class="keyboard__key" data="${keyCode[i]}">${keys[i]}</div>`;
-  }
-  document.querySelector('.keyboard').innerHTML = out;
+let out = '';
+for (let i = 0; i < keysEng.length; i++) {
+  out += `<div class="keyboard__key" data="${keyCode[i]}">
+            <span class="key eng small">${keysEng[i]}</span>
+            <span class="key rus small hidden">${keysRus[i]}</span>
+          </div>`;
 }
-init();
+document.querySelector('.keyboard').innerHTML = out;
+
+let caps = false;
 
 function getClass() {
-  const allkeys = document.querySelectorAll('.keyboard__key');
-  // eslint-disable-next-line no-plusplus
-  for (let i = 0; i < allkeys.length; i++) {
-    if (allkeys[i].textContent === 'Backspace' || allkeys[i].innerHTML === '' || allkeys[i].innerHTML === 'CapsLock'
-    || allkeys[i].innerHTML === 'Enter' || allkeys[i].innerHTML === 'Shift') {
-      allkeys[i].classList.add('key_wide');
+  for (let i = 0; i < document.querySelectorAll('.keyboard__key').length; i++) {
+    if (document.querySelectorAll('.keyboard__key')[i].getAttribute('data') === 'Backspace') {
+      document.querySelectorAll('.keyboard__key')[i].classList.add('key_wide');
     }
-    if (allkeys[i].textContent === 'Space') {
-      allkeys[i].classList.add('key_superwide');
+    if (document.querySelectorAll('.keyboard__key')[i].getAttribute('data') === 'CapsLock') {
+      document.querySelectorAll('.keyboard__key')[i].classList.add('key_wide');
+    }
+    if (document.querySelectorAll('.keyboard__key')[i].getAttribute('data') === 'ShiftLeft') {
+      document.querySelectorAll('.keyboard__key')[i].classList.add('key_wide');
+    }
+    if (document.querySelectorAll('.keyboard__key')[i].getAttribute('data') === 'Enter') {
+      document.querySelectorAll('.keyboard__key')[i].classList.add('key_wide');
+    }
+    if (document.querySelectorAll('.keyboard__key')[i].getAttribute('data') === 'Space') {
+      document.querySelectorAll('.keyboard__key')[i].classList.add('key_superwide');
     }
   }
 }
 
 getClass();
 
-const allKeys = document.querySelectorAll('.keyboard__key');
-
-// eslint-disable-next-line func-names
-document.onkeydown = function (event) {
-  allKeys.forEach((element) => {
-    element.classList.remove('key_active');
-  });
-  document.querySelector(`.keyboard__key[data="${event.code}"]`).classList.add('key_active');
-};
-
-allKeys.forEach((element) => {
-  element.addEventListener('click', () => {
-    allKeys.forEach((elem) => {
-      elem.classList.remove('key_active');
+function listener() {
+  document.onkeydown = function (event) {
+    document.querySelectorAll('.keyboard__key').forEach((element) => {
+      element.classList.remove('key_active');
     });
-    element.classList.add('key_active');
+    document.querySelector(`.keyboard__key[data="${event.code}"]`).classList.add('key_active');
+    text.focus();
+    text.focus();
+    switch (event.key) {
+      case 'Enter':
+        text.value += '\n';
+        break;
+      case 'Del':
+        text.value = text.value.slice(0, text.value[text.selectionStart]) + text.value.slice(text.value[text.selectionStart 
+          + 1]);
+        break;
+      case 'Backspace':
+        text.value = text.value.slice(0, text.value.length - 1);
+        break;
+      case 'Tab':
+        text.value += '    ';
+        break;
+      case 'Space':
+        text.value += ' ';
+        break;
+      case 'Shift':
+      case 'Alt':
+      case 'Ctrl':
+      case 'Win':
+        text.value += '';
+        break;
+      case 'CapsLock':
+
+        break;
+      default:
+        if (!event.target.classList.contains('hidden')) {
+          text.value += event.target.textContent;
+        }
+    }
+    text.value += event.key;
+    console.log(event);
+  };
+
+  document.querySelectorAll('.key').forEach((element) => {
+    element.addEventListener('click', () => {
+      document.querySelectorAll('.key').forEach((elem) => {
+        elem.classList.remove('key_active');
+      });
+      element.classList.add('key_active');
+      text.focus();
+      switch (element.textContent) {
+        case 'Enter':
+          text.value += '\n';
+          break;
+        case 'Del':
+          text.value = text.value.slice(0, text.value[text.selectionStart]);
+          break;
+        case 'Backspace':
+          text.value = text.value.slice(0, text.value.length - 1);
+          break;
+        case 'Tab':
+          text.value += '    ';
+          break;
+        case 'Space':
+          text.value += ' ';
+          break;
+        case 'Shift':
+        case 'Alt':
+        case 'Ctrl':
+        case 'Win':
+          text.value += '';
+          break;
+        case 'CapsLock':
+          if (caps === false && element.classList.contains('eng')) {
+            caps = true;
+            let out = '';
+            for (let i = 0; i < keysEng.length; i++) {
+              out += `<div class="keyboard__key" data="${keyCode[i]}">
+                <span class="key eng">${keysEng[i].length === 1 ? keysEng[i].toUpperCase() : keysEng[i]}</span>
+                <span class="key rus hidden">${keysRus[i]}</span>
+              </div>`;
+            }
+            document.querySelector('.keyboard').innerHTML = out;
+          } else if (caps === true && element.classList.contains('eng')) {
+            caps = false;
+            let out = '';
+            for (let i = 0; i < keysEng.length; i++) {
+              out += `<div class="keyboard__key" data="${keyCode[i]}">
+                <span class="key eng">${keysEng[i].length === 1 ? keysEng[i].toLowerCase() : keysEng[i]}</span>
+                <span class="key rus hidden">${keysRus[i]}</span>
+              </div>`;
+            }
+            document.querySelector('.keyboard').innerHTML = out;
+          }
+          if (caps === false && element.classList.contains('rus')) {
+            caps = true;
+            let out = '';
+            for (let i = 0; i < keysEng.length; i++) {
+              out += `<div class="keyboard__key" data="${keyCode[i]}">
+                <span class="key eng  hidden">${keysEng[i]}</span>
+                <span class="key rus">${keysRus[i].length === 1 ? keysRus[i].toUpperCase() : keysRus[i]}</span>
+              </div>`;
+            }
+            document.querySelector('.keyboard').innerHTML = out;
+          } else if (caps === true && element.classList.contains('rus')) {
+            caps = false;
+            let out = '';
+            for (let i = 0; i < keysEng.length; i++) {
+              out += `<div class="keyboard__key" data="${keyCode[i]}">
+                <span class="key eng hidden">${keysEng[i]}</span>
+                <span class="key rus">${keysRus[i].length === 1 ? keysRus[i].toLowerCase() : keysRus[i]}</span>
+              </div>`;
+            }
+            document.querySelector('.keyboard').innerHTML = out;
+          }
+          getClass();
+          listener();
+          break;
+        default:
+          if (!element.classList.contains('hidden')) {
+            if (caps === false) {
+              text.value += element.textContent.toLowerCase();
+            } else {
+              text.value += element.textContent.toUpperCase();
+            }
+          }
+      }
+    });
   });
-});
+}
+getClass();
+listener();
